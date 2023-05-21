4988c0d1264c086a81cec98b5c3b23bbe1c751bd
Sandeep Kumar <sandeepkumar.er21@gmail.com>
2023-05-21 16:32:02 -0700

    FMT

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.4.6"
   required_providers {
     intersight = {
-      source = "CiscoDevNet/intersight"
+      source  = "CiscoDevNet/intersight"
       version = "~> 1.0.36"
     }
   }
