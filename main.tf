38002e48d7ea0ea470029ccabac71bedc1e9802f
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2024-10-20 18:20:16 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.4.6"
+  required_version = "~> 1.9.8"
   required_providers {
     intersight = {
       source  = "CiscoDevNet/intersight"
-      version = "~> 1.0.36"
+      version = "~> 1.0.57"
     }
   }
 }
